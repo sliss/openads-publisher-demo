dd3124e951d992139bed933df32bb94b3e21c28c
Steven Liss <stevenliss45@gmail.com>
2025-02-18 21:11:01 -0500

    contextual dashboard is pretty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  safelist: [
+    'bg-green-50',
+    'bg-yellow-50',
+    'bg-orange-50',
+    'bg-red-50',
+    'text-green-700',
+    'text-yellow-700',
+    'text-orange-700',
+    'text-red-700',
+    'bg-[#d67238]/5',
+    'text-[#d67238]',
+    {
+      pattern: /bg-(green|yellow|orange|red)-(50|700)/,
+      variants: ['hover', 'focus'],
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
